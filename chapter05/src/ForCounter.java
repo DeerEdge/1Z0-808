07631836d93665c4383cc9d5d902c7ac85e4bb76
DeerEdge <Vislawath.21@gmail.com>
2023-01-16 14:17:48 -0700

    Chapter 5 Completed

---
+++ chapter05/src/ForCounter.java
@@ -0,0 +1,26 @@
+public class ForCounter
+{
+    public static void main(String[] args)
+    {
+        // for loop continues till condition is no longer met
+        for (int counter = 1; counter <= 10; counter++)
+            System.out.printf("%d ", counter);
+
+        System.out.println();
+    }
+}
+
+/*
+    for loop:
+    for(initialization; loopContinuationCondition; increment)
+        statement
+
+    while loop:
+    initialization;
+    while (loopContinuationCondition)
+    {
+        statement
+        increment
+    }
+
+ */
